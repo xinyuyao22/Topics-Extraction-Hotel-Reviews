fd35484d2dc43cba26c9a93c62110484128feb2e
xinyuyao22 <54604406+xinyuyao22@users.noreply.github.com>
2020-04-25 23:06:06 -0400

    sentence classification

--- get_aspect_train_test.py
+++ get_aspect_train_test.py
@@ -1,27 +1,25 @@
 import json
 import csv
 
-filepath = 'C:/NLP/hotel-reviews/outdir/aspects-annotated-dataset/tripadvisor'
+filepath = 'C:/Users/xyyao/Documents/GitHub/Topics-Extraction-Hotel-Reviews/hotel-reviews/aspects-annotated-dataset/tripadvisor'
 
 def write_csv(file):
-    i = 0
     for line in open(file, 'r', encoding="utf-8"):
-        print(i)
         line = json.loads(line)
-        print(line)
         for label, sent in zip(line['segmentLabels'],line['segments']):
             if bool(label):
-                    writer.writerow([sent, str(list(label.keys())[0])])
-        i += 1
-with open('train_unique.csv', 'w', newline='', encoding="utf-8") as file:
+                writer.writerow([sent, list(label.keys())])
+
+
+with open('aspect_train.csv', 'w', newline='', encoding="utf-8") as file:
     writer = csv.writer(file)
     writer.writerow(["Sentence", "Aspect"])
     write_csv(filepath + '/train.unique.json')
-
-with open('test_combined.csv', 'w', newline='', encoding="utf-8") as file:
-    writer = csv.writer(file)
-    writer.writerow(["Sentence", "Aspect"])
-    write_csv(filepath + '/test.unique.json')
     write_csv(filepath + '/test.zero.json')
     write_csv(filepath + '/test.one.json')
     write_csv(filepath + '/test.two.json')
+
+with open('aspect_test.csv', 'w', newline='', encoding="utf-8") as file:
+    writer = csv.writer(file)
+    writer.writerow(["Sentence", "Aspect"])
+    write_csv(filepath + '/test.unique.json')
